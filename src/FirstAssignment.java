a5d00f12a200e0d4a3c39e36f5b440963fb9a7eb
Naufal <naufal.ti@outlook.com>
2022-03-04 15:07:21 +0700

    first commit

---
+++ src/FirstAssignment.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FirstAssignment {
+
+    public static void main(String[] args){
+        List<Integer> numberIntegers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        // Given a list of integers, find out all the even numbers exist in the list using Stream functions
+        List<Integer> evenNumberIntegers = numberIntegers.stream()
+                                            .filter(n -> n%2 == 0)
+                                            .collect(Collectors.toList());
+        System.out.println(evenNumberIntegers);
+
+        // Given a list of integers, find out all the numbers starting with 1 using Stream functions
+        List<Integer> startWith1Integers = numberIntegers.stream()
+                                            .filter(n -> n.toString().startsWith("1"))
+                                            .collect(Collectors.toList());
+        System.out.println(startWith1Integers);
+
+        // Given a list of integers, find the total number of elements present in the list using Stream functions
+        Long numberOfElements = numberIntegers.stream()
+                                            .count();
+        System.out.println(numberOfElements);
+
+        // Given a list of integers, sort all the values present in it using Stream functions
+        List<Integer> randNumIntegers = Arrays.asList(4, 2, 7, 1, 9);
+        List<Integer> sortedNumIntegers = randNumIntegers.stream()
+                                            .sorted()
+                                            .collect(Collectors.toList());
+
+        System.out.println("Before sorting: " + randNumIntegers +
+                            "\nAfter sorting: " + sortedNumIntegers);
+
+    }
+
+
+
+
+    
+}
---
+++ src/SecondAssignment.java
@@ -0,0 +1,111 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import model.Invoice;
+
+public class SecondAssignment {
+
+    public static void main(String[] args){
+        String[] customerName = {"John", "Max", "Phil", "Roger", "Yae", "Doe", "William"};
+
+        List<Invoice> invoices = AddDummyData();
+        
+        List<Invoice> oracleAndTrainingInvoices = invoices.stream()
+                                                    .filter(invoice -> {
+                                                        for (String name : customerName) {
+                                                            if(invoice.getCustomer() == name){
+                                                                return true;
+                                                            }
+                                                        }
+                                                        return false;
+                                                    })
+                                                    .filter(invoice -> invoice.getTitle().contains("Training"))
+                                                    .sorted(Comparator.comparingInt(Invoice::getAmount)
+		                                            .reversed())
+                                                    .collect(Collectors.toList());
+
+        List<Integer> ids = oracleAndTrainingInvoices.stream()
+                            .map(invoice -> invoice.getId())
+                            .collect(Collectors.toList());
+
+        List<Integer> firstFiveIds = oracleAndTrainingInvoices.stream()
+                            .map(invoice -> invoice.getId())
+                            .limit(5)
+                            .collect(Collectors.toList());
+        
+        System.out.println(ids);
+        System.out.println(firstFiveIds);
+
+    }
+
+    public static List<Invoice>  AddDummyData() {
+        List<Invoice> invoices = new ArrayList<>();
+
+        invoices.add(new Invoice());
+        invoices.get(0).setCustomer("John");
+        invoices.get(0).setId(1);
+        invoices.get(0).setAmount(100);
+        invoices.get(0).setTitle("Training");
+        
+        invoices.add(new Invoice());
+        invoices.get(1).setCustomer("Max");
+        invoices.get(1).setId(2);
+        invoices.get(1).setAmount(200);
+        invoices.get(1).setTitle("Training");
+
+        invoices.add(new Invoice());
+        invoices.get(2).setCustomer("Phil");
+        invoices.get(2).setId(3);
+        invoices.get(2).setAmount(100);
+        invoices.get(2).setTitle("Training");
+
+        invoices.add(new Invoice());
+        invoices.get(3).setCustomer("Roger");
+        invoices.get(3).setId(4);
+        invoices.get(3).setAmount(110);
+        invoices.get(3).setTitle("Training");
+
+        invoices.add(new Invoice());
+        invoices.get(4).setCustomer("Doe");
+        invoices.get(4).setId(5);
+        invoices.get(4).setAmount(100);
+        invoices.get(4).setTitle("Training");
+
+        invoices.add(new Invoice());
+        invoices.get(5).setCustomer("Yae");
+        invoices.get(5).setId(6);
+        invoices.get(5).setAmount(100);
+        invoices.get(5).setTitle("Training");
+
+        invoices.add(new Invoice());
+        invoices.get(6).setCustomer("William");
+        invoices.get(6).setId(7);
+        invoices.get(6).setAmount(400);
+        invoices.get(6).setTitle("Training");
+
+        invoices.add(new Invoice());
+        invoices.get(7).setCustomer("Sumito");
+        invoices.get(7).setId(8);
+        invoices.get(7).setAmount(100);
+        invoices.get(7).setTitle("Master");
+
+        invoices.add(new Invoice());
+        invoices.get(8).setCustomer("Phil");
+        invoices.get(8).setId(9);
+        invoices.get(8).setAmount(900);
+        invoices.get(8).setTitle("Master");
+
+        invoices.add(new Invoice());
+        invoices.get(9).setCustomer("Robert");
+        invoices.get(9).setId(10);
+        invoices.get(9).setAmount(1000);
+        invoices.get(9).setTitle("Master");
+
+        return invoices;
+        
+    }
+   
+    
+}
